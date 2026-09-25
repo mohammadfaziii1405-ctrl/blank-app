254a2115eb3d75c7ebc3b5fe73a38c510059919b
mohammadfaziii1405-ctrl <mohammadfaziii1405@gmail.com>
2026-09-25 15:51:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+        import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Expense Tracker", layout="centered")
@@ -6,14 +6,14 @@ st.set_page_config(page_title="Expense Tracker", layout="centered")
 st.title("Professional Expense Tracker")
 st.divider()
 
-amount = st.number_input("Amount", min_value=0.0, format="%.2f")
-category = st.selectbox("Category", ["Food", "Travel", "Shopping", "Bills", "Other"])
+amount = st.number_input("Amount", min_value=1)
+category = st.selectbox("Category", ["Food", "Travel", "Shopping", "Bills"])
 date = st.date_input("Date")
 
 if st.button("Add Expense"):
-    st.success(f"Added: {category} - Rs. {amount} on {date}")
+    st.success(f"Added: {category} - Rs. {amount}")
     st.balloons()
 
 st.divider()
-st.caption("Built with Streamlit | Professional Version")                                                 
-                                                                                                                                               st.error("Ye symbol nahi mila! .NS laga                                                                                                                  
+st.caption("Built with Streamlit | Professional App")
+                                                                                                                                               st.error("Ye symbol nahi mila! .NS laga                                                         
