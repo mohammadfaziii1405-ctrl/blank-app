798958268f5837d7f33d018e9fd4eb2201f1157b
mohammadfaziii1405-ctrl <mohammadfaziii1405@gmail.com>
2026-09-25 19:07:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,22 @@ import streamlit as st
 import pandas as pd
 
 st.title("StockWise - Mera App")
+st.header("GDP Comparison 2024")
+
+data = {
+    "Country": ["India (IND)", "Germany (DEU)"],
+    "GDP": [3913, 4660]
+}
+
+df = pd.DataFrame(data)
+st.bar_chart(df.set_index("Country"))
+
+st.write("India GDP: $3,913 Billion")
+st.write("Germany GDP: $4,660 Billion")
+st.success("App ab sahi kaam karega!")import streamlit as st
+import pandas as pd
+
+st.title("StockWise - Mera App")
 
 st.header("GDP Comparison 2024")
 
