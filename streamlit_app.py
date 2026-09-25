38e36b2ca86d1941004f6339bce4bbc0cad07285
mohammadfaziii1405-ctrl <mohammadfaziii1405@gmail.com>
2026-09-25 19:06:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
-st.title("Kotpura Expense Tracker")
-st.success("App Working!")
-st.write("Welcome to Kotpura")
-st.balloons()
+import pandas as pd
+
+st.title("StockWise - Mera App")
+
+st.header("GDP Comparison 2024")
+
+# Data jo aapne nikala tha
+data = {
+    "Country": ["India (IND)", "Germany (DEU)"],
+    "GDP": [3913, 4660]
+}
+
+df = pd.DataFrame(data)
+st.bar_chart(df.set_index("Country"))
+
+st.write("🇮🇳 India GDP: $3,913 Billion")
+st.write("🇩🇪 Germany GDP: $4,660 Billion")
+st.success("App ab sahi kaam karega!")
                                                                                                                                                st.error("Ye symbol nahi mila! .NS laga                                                         
