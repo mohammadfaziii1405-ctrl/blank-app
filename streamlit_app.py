ff04f9d4aa3ef3957877a1ffc076e7e09b3252bb
mohammadfaziii1405-ctrl <mohammadfaziii1405@gmail.com>
2026-09-25 19:11:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,23 +14,4 @@ st.bar_chart(df.set_index("Country"))
 
 st.write("India GDP: $3,913 Billion")
 st.write("Germany GDP: $4,660 Billion")
-st.success("App ab sahi kaam karega!")import streamlit as st
-import pandas as pd
-
-st.title("StockWise - Mera App")
-
-st.header("GDP Comparison 2024")
-
-# Data jo aapne nikala tha
-data = {
-    "Country": ["India (IND)", "Germany (DEU)"],
-    "GDP": [3913, 4660]
-}
-
-df = pd.DataFrame(data)
-st.bar_chart(df.set_index("Country"))
-
-st.write("🇮🇳 India GDP: $3,913 Billion")
-st.write("🇩🇪 Germany GDP: $4,660 Billion")
-st.success("App ab sahi kaam karega!")
-                                                                                                                                               st.error("Ye symbol nahi mila! .NS laga                                                         
+st.success("App ab sahi kaam karega!")        
